0621426a84a966e09d4ec267d0bdf56c44d39c90
Gumpah <vickinzi@yahoo.fr>
2022-03-10 18:24:07 +0100

    Login activity xml done

---
+++ app/src/main/java/fr/barrow/go4lunch/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package fr.barrow.go4lunch.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.barrow.go4lunch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
