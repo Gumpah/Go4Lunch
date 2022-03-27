2fbd412b3a2050e1d0c9bf25c9b80c58fd59b367
Gumpah <vickinzi@yahoo.fr>
2022-03-27 23:27:37 +0200

    Adding toolbar + bottom navigation view + drawer menu

--- app/src/main/java/fr/barrow/go4lunch/ui/MainActivity.java
+++ app/src/main/java/fr/barrow/go4lunch/ui/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
@@ -13,22 +11,31 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import fr.barrow.go4lunch.R;
 import fr.barrow.go4lunch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private AppBarConfiguration mAppBarConfiguration;
+    private NavController mNavController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initUI();
+        initAppBarConfiguration();
+        initNavController();
         initToolbar();
         initDrawer();
         initBottomNavigationView();
+    }
+
+    private void initAppBarConfiguration() {
+        mAppBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.navigation_mapView, R.id.navigation_listView, R.id.navigation_workmates)
+                        .setOpenableLayout(binding.drawerLayout)
+                        .build();
     }
 
     private void initUI() {
@@ -37,27 +44,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
     }
 
-    private void initDrawer() {
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, binding.drawerLayout, binding.activityMainToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        binding.drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-    }
-
-    private void initToolbar() {
-        setSupportActionBar(binding.activityMainToolbar);
+    private void initNavController() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_host_fragment);
+        mNavController = navHostFragment.getNavController();
     }
 
     private void initBottomNavigationView() {
-        BottomNavigationView navView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_mapView, R.id.navigation_listView, R.id.navigation_workmates)
-                .build();
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_host_fragment);
-        NavController navController = navHostFragment.getNavController();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(binding.bottomNavViewMainNav, mNavController);
+
+    }
+
+    private void initToolbar() {
+        NavigationUI.setupWithNavController(binding.activityMainToolbar, mNavController, mAppBarConfiguration);
+
+    }
+
+    private void initDrawer() {
+        NavigationUI.setupWithNavController(binding.navigationViewSideMenu, mNavController);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, mNavController)
+                || super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -67,23 +76,5 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.mainMenuDrawer_lunch:
-                break;
-            case R.id.mainMenuDrawer_settings:
-                break;
-            case R.id.mainMenuDrawer_logout:
-                break;
-            default:
-                break;
-        }
-
-        binding.drawerLayout.closeDrawer(GravityCompat.START);
-
-        return super.onOptionsItemSelected(item);
     }
 }
